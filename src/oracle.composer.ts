e7c22e68d343055689495edebc915b3dd85da587
songtianyi <tianyi@saas3.io>
2022-08-11 21:38:51 +0800

    update

--- src/oracle.composer.ts
+++ src/oracle.composer.ts
@@ -14,16 +14,16 @@ const sponsorMnemonic = "aisle genuine false door mouse sustain caught flock pyr
 const sponsor = "0x944e24Ded49747c8278e3D3b4148da68e5B6672C";
 const sponsorWallet = "0xdb2E1351c5De993629e703b51A730D7A6Ed24271"
 // saas3
-const provider = "http://150.109.145.144:9101"
-const airnodeRrp = "0xebd9ffD45a96f01D7Aa334d4b45dF70eF2cad31d";
-const chainId = "1280";
-const network = "saas3-testnet";
+//const provider = "http://150.109.145.144:9101"
+//const airnodeRrp = "0xebd9ffD45a96f01D7Aa334d4b45dF70eF2cad31d";
+//const chainId = "1280";
+//const network = "saas3-testnet";
 
 // moonbeam alpha
-// const airnodeRrp = "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd"
-// const provider = "https://rpc.api.moonbase.moonbeam.network";
-// const chainId = "1287";
-// const network = "Moonbase Alpha";
+const airnodeRrp = "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd"
+const provider = "https://rpc.api.moonbase.moonbeam.network";
+const chainId = "1287";
+const network = "Moonbase Alpha";
 
 // Rinkeby
 // const airnodeRrp = "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd"
@@ -110,8 +110,9 @@ export async function deployWithWeb3(abi: any, bytecode: any) {
         {
             data: incrementerTx.encodeABI(),
             gas: await incrementerTx.estimateGas(),
+            //gasPrice: web3.utils.toWei('1000', 'gwei'),
         },
-        accountFrom.privateKey
+        accountFrom.privateKey,
     );
     const receipt = await web3.eth.sendSignedTransaction(tx.rawTransaction);
     console.log(`Contract deployed at address: ${receipt.contractAddress}`);
